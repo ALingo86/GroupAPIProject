62d0cae361936864a3355aa6469861bdd06c2dd2
jorwood1998 <jorwood1998@gmail.com>
2022-01-24 11:08:26 -0500

    added gitignore/exotic/petfriendly

---
+++ PetAdopterAPI/Models/isPetFriendlyExotic.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace PetAdopterAPI.Models
+{
+    public class isPetFriendlyExotic
+    {
+        [ForeignKey(nameof(ExoticTable))]
+        public bool IsPetFriendly { get; set; }
+    }
+}
