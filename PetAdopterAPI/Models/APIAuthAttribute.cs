3b0ecc050612ceefde92acb3c1a84437de17fc42
jorwood1998 <jorwood1998@gmail.com>
2022-01-25 11:18:44 -0500

    ApiAuthDone

--- PetAdopterAPI/Models/APIAuthAttribute.cs
+++ PetAdopterAPI/Models/APIAuthAttribute.cs
@@ -5,7 +5,18 @@ using System.Web;
 
 namespace PetAdopterAPI.Models
 {
-    public class APIAuthAttribute
+    public class APIAUTHORIZEATTRIBUTE : System.Web.Http.AuthorizeAttribute
     {
+        protected override void HandleUnauthorizedRequest(System.Web.Http.Controllers.HttpActionContext actionContext)
+        {
+            if (!HttpContext.Current.User.Identity.IsAuthenticated)
+            {
+                base.HandleUnauthorizedRequest(actionContext);
+            }
+            else
+            {
+                actionContext.Response = new System.Net.Http.HttpResponseMessage(System.Net.HttpStatusCode.Forbidden);
+            }
+        }
     }
 }
